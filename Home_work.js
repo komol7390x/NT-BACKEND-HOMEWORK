3dc1253d672a50163d32f7571770a4f78c3cc1bf
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-05-26 17:50:39 +0500

    u[

--- Home_work.js
+++ Home_work.js
@@ -1,13 +1,28 @@
+// CALL
 // TASK-1
 function greet() {
     console.log(`Salom, mening ismim ${this.name}`);
 }
 const user = { name: "Ali" };
-// (greet.call(user));
+(greet.call(user));
 // ---------------------------------------------------------
 // TASK-2
 function introduce(age, job) {
     console.log(`Men ${this.name}, yoshim ${age}, kasbim ${job}.`);
 }
 const person = { name: "Shahzod"};
-introduce.call(person,24,"Developer")
+introduce.call(person, 24, "Developer")
+// ---------------------------------------------------------
+// TASK-3
+const teacher = {
+    name: "Asad",
+    subject: "Matematika",
+    introduce() {
+        console.log(`Men ${this.name}, ${this.subject} o‘qituvchisiman.`);
+    }
+};
+const anotherTeacher = { name: "Karim", subject: "Fizika" };
+teacher.introduce(); //o'zini chaqirish;
+teacher.introduce.call(anotherTeacher);  //anotherTeacher orqali chaqirish;
+// ---------------------------------------------------------
+// TASK-5
