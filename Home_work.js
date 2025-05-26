68a016be835b12666f474ce4c939a189846158e3
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-05-26 17:41:57 +0500

    up

---
+++ Home_work.js
@@ -0,0 +1,9 @@
+// TASK-1
+function greet() {
+    console.log(`Salom, mening ismim ${this.name}`);
+}
+const user = { name: "Ali" };
+// (greet.call(user));
+// ---------------------------------------------------------
+// TASK-2
+
