fbefb42b8172399c8f09da3cab3d0977b7cc22bc
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-05-26 20:33:56 +0500

    up

---
+++ Home_work-2.js
@@ -0,0 +1,40 @@
+// task-1
+let num1 = 5.33234
+console.log(num1.toFixed(2));
+// ----------------------------------
+// task-2
+console.log(num1.toPrecision(4));
+// ----------------------------------
+// task-3
+let num2 = 12345
+console.log(num2.toString(16));
+// ----------------------------------
+// task-4
+let str1 = "42px"
+console.log(parseInt(str1));
+// ----------------------------------
+// task-5
+let str2 = "3.14m"
+console.log(parseFloat(str2));
+// ----------------------------------
+// task-6
+let str3="qwerty"
+if (Number.isNaN != str3) {
+    console.log("Bu son emas");
+}
+// ----------------------------------
+// task-7
+let num3 =Infinity
+if (num3 != isFinite) {
+    console.log("Bu cheksiz son!");
+}
+// ----------------------------------
+// task-8 
+let num4 = -125;
+console.log(Math.abs(num4));
+// ----------------------------------
+// task-9
+console.log(Math.pow(2,9));
+// ----------------------------------
+// task-10
+
