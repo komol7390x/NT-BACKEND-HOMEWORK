d7435d3b3377ed6c6a431b763d73a242fd239ae0
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-05-27 17:53:05 +0500

    up

---
+++ asosiy-1.js
@@ -0,0 +1,7 @@
+// TASK-1
+let arr1 = [1, 4, 7, 10, 3, 8];
+let arr2 = arr1.filter(x => x % 2 == 0)
+console.log(arr2);
+// ------------------------------------------
+// TASK-2
+let arr3 = [2, 3, 5]  
