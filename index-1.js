39b3673c003e48ebb50a99d19ec348ced717b980
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-05-28 20:20:07 +0500

    up

--- index-1.js
+++ index-1.js
@@ -40,9 +40,66 @@ map1.set('a',1).set("b",2).set("c",3)
 // --------------------------------------------------------
 // TASK-4
 function sortMap(map) {
-    
+    let map2 = new Map([...map].sort())
+    return map2
 }
-let map2 = new Map([["banana", 2], ["apple", 3], ["orange", 1], ["anor", 4]])
-console.log(sortMap(map2));
+let map2 = new Map([["banana", 2], ["apple", 3], ["orange", 1], ["anor", 4], ["ananas", 5]])
+// console.log(sortMap(map2));
+// --------------------------------------------------------
+// TASK-5
+function addMap(map1, map2) {
+    for (let [key, val] of map1) {
+        if (!map2.has(val)) 
+            map2.set(key, val)
+    }
+    return map2
+}
 
+const map3 = new Map([
+    ['a', 1],
+    ['i', 1],
+    ['b', 2],
+    ['c', 3],
+    ['e', 4]
+]);
+
+const map4 = new Map([
+    ['b', 20],
+    ['g', 20],
+    ['c', 30],
+    ['d', 40],
+    ['f', 40]
+]);
+// console.log(addMap(map3,map4));
+// --------------------------------------------------------
+// TASK-6
+function findNumber(map) {
+    for (let [key, val] of map) {
+        if (typeof val == "number") {
+            map.set(key,val*2)
+        }
+    }
+    return map
+}
+const num2 = new Map([
+    ['a', 10],
+    ['b', 'salom'],
+    ['c', 5],
+    ['d', true],
+    ['e', 3]
+]);
+// console.log(findNumber(num2));
+// --------------------------------------------------------
+// TASK-7
+function changeValue(map) {
+    let map2=new Map()
+    for (let [key, val] of map) {
+        map2.set(val,key)
+    }
+    return map2
+}
+const map5 = new Map([["x", 1], ["y", 2], ["z", 3]]);
+console.log(changeValue(map5));
+// --------------------------------------------------------
+// TASK-8
 
