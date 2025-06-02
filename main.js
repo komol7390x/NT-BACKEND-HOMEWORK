57f3cc34d35d93db290272a7f2fabfc9dff87897
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-06-02 17:50:56 +0500

    up

--- main.js
+++ main.js
@@ -7,8 +7,63 @@ Car1.prototype.getinfo=function(){
 const car = new Car1();
 // console.log(car.getinfo())
 // ----------------------------------------------------------------------
-Animal = {
+const Animal = {
     makeSound() {
         console.log("Some sound")
     }
+};
+const Dog = {
+    bark() {
+        console.log("Woof"); 
+   }
+};
+Dog.__proto__ = Animal;
+// Dog.makeSound()
+// Dog.bark()
+// ----------------------------------------------------------------------
+Array.prototype.sum = function () {
+    return arr.reduce((acc, item) => acc += item, 0);
 }
+const arr = [1, 2, 3, 4, 5]
+// console.log(arr.sum());
+// ----------------------------------------------------------------------
+function Person(name) {
+    this.name=name
+}
+Person.prototype.greet = function () {
+    (console.log("Hello,my name is " + this.name));
+}
+function Employee(name) {
+    this.name = name;
+}
+Employee.prototype.__proto__ = Person.prototype;
+let res = new Employee("Komol");
+// res.greet()
+// ----------------------------------------------------------------------
+let Car = {
+    brand:"BMW",
+    speed:100,
+    drive() {
+        console.log(this.brand + " is driving at " + this.speed + " km / h")
+    }
+}
+// Car.drive()
+let sportCar = Object.create(Car);
+sportCar.speed = 200;
+// sportCar.drive()
+// ----------------------------------------------------------------------
+class Book{
+    constructor(title, author, year) {
+        this.title = title;
+        this.author = author;
+        this.year = year;
+    }
+    getSummary() {
+        console.log(`Title: ${this.title}, Author: ${this.author}, Year: ${this.year}`);
+    }
+}
+const res2 = new Book("The Alchemist", "Paulo Coelho", 1988);
+res2.getSummary()
+let res3 = new Book("Shum Bola", "Gafur Gulam", 1960);
+res3.getSummary();
+// ----------------------------------------------------------------------
