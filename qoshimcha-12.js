747d46b28c43d2dc904c22d2fddcbee42c615dd3
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-06-03 21:17:18 +0500

    up

--- qoshimcha-12.js
+++ qoshimcha-12.js
@@ -1,10 +1,11 @@
 class Library{
-    #book=new Map()
-    addBook() {
-        
+    #book = new Map()
+    addBook(name, descriptor, author, year, genre) {
+        res = new Book(name, descriptor, author, year, genre)
+        this.#book.set(this.#book.size+1,res)
     }
     showBook() {
-        
+        console.log(this.#book);
     }
     showBookId() {
         
@@ -25,5 +26,10 @@ class Book{
         this.genre = genre
     }
 }
-let res = new Book("O'tgan kunlar", "Muxabbat haqida", "Abdulla Qodiry", 1920, "drama");
-console.log(res);
+let res = new Book();
+let lib = new Library();
+lib.addBook("O'tkan kunlar", "Eski davr muhabbat fojia", "Abdulla Qodiriy", 1926, "Tarixiy roman")
+lib.addBook("Mehrobdan chayon", "Xiyonat sevgi ayb", "Pirimqul Qodirov", 1972, "Dramatik roman");
+lib.addBook("Ufq", "Qishloq hayoti islohotlar", "Said Ahmad", 1980, "Ijtimoiy roman");
+lib.addBook("Ko'hna dunyo", "Tarix haqiqat insoniyat", "Erkin A'zam", 2005, "Tarixiy esse");
+// lib.showBook()
