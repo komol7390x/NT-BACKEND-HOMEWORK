3c749661bdd4b2bee0c1e65802b238ba0fbec63c
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-06-03 21:44:28 +0500

    up

--- qoshimcha-12.js
+++ qoshimcha-12.js
@@ -5,16 +5,40 @@ class Library{
         this.#book.set(this.#book.size+1,res)
     }
     showBook() {
-        console.log(this.#book);
+        for (let [key, val] of this.#book.entries()) {
+            console.log(`ID: ${key} \nNomi: ${val.name} \nYozuvchi: ${val.author}\nHaqida: ${val.descriptor}\nJanr: ${val.genre}\nYili: ${val.year}\n-----------------------`);
+        }
     }
-    showBookId() {
-        
+    showBookId(id) {
+        for (let [key, val] of this.#book.entries()) {
+            if (key == id) {
+                console.log(`ID: ${key} \nNomi: ${val.name} \nYozuvchi: ${val.author}\nHaqida: ${val.descriptor}\nJanr: ${val.genre}\nYili: ${val.year}\n`);
+            }
+        }
     }
-    update() {
-        
+    update(id,genre) {
+        for (let [key, val] of this.#book.entries()) {
+            if (key == id) {
+                val.genre = genre;
+                console.log("Taxrirlandi\n");
+                console.log(`ID: ${key} \nNomi: ${val.name} \nYozuvchi: ${val.author}\nHaqida: ${val.descriptor}\nJanr: ${val.genre}\nYili: ${val.year}\n`);
+            }
+        }
     }
-    delete() {
-        
+    delete(name) {
+        for (let [key, val] of this.#book.entries()) {
+            if (val.name==name) {
+                this.#book.delete(key)
+                console.log(`ID: ${key} \nNomi: ${val.name} \nO'chirildi\n`);
+            }
+        }
+    }
+    searchBook(name) {
+        for (let [key, val] of this.#book.entries()) { 
+            if (val.name.includes(name)) {
+                console.log(`ID: ${key} \nNomi: ${val.name} \nYozuvchi: ${val.author}\nHaqida: ${val.descriptor}\nJanr: ${val.genre}\nYili: ${val.year}\n`);
+            }
+        }
     }
 }
 class Book{
@@ -31,5 +55,10 @@ let lib = new Library();
 lib.addBook("O'tkan kunlar", "Eski davr muhabbat fojia", "Abdulla Qodiriy", 1926, "Tarixiy roman")
 lib.addBook("Mehrobdan chayon", "Xiyonat sevgi ayb", "Pirimqul Qodirov", 1972, "Dramatik roman");
 lib.addBook("Ufq", "Qishloq hayoti islohotlar", "Said Ahmad", 1980, "Ijtimoiy roman");
-lib.addBook("Ko'hna dunyo", "Tarix haqiqat insoniyat", "Erkin A'zam", 2005, "Tarixiy esse");
+lib.addBook("Ko'hna dunyo", "Tarix haqiqat insoniyat", "Erkin A'zam", 2005, "Tarixiy hikoya");
 // lib.showBook()
+// lib.showBookId(2)
+// lib.update(2,"Roman")
+// lib.delete("Ufq");
+// lib.showBook()
+lib.searchBook("d")
