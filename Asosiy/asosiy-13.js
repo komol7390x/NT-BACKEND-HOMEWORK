4dca78e323b8a3d35d08faaaafb133ec7ba57815
komol7390x <www.komol8689@gmail.com>
2025-06-09 16:47:31 +0500

    up

--- Asosiy/asosiy-13.js
+++ Asosiy/asosiy-13.js
@@ -1,10 +1,19 @@
-const email = document.getElementById('email')
-const pass = document.getElementById('password');
-const btn = getElementById('btn');
-const h1=document.createElement('h1')
-const showResult = () => {
-    const res = `Login: ${email} <br>Password: ${pass}`
-    res.style.fontSize="50px"
-    h1.appendChild(res)
-}
-document.body.appendChild(h1)
+ const email = document.getElementById('email');
+    const pass = document.getElementById('password');
+    const btn = document.getElementById('btn');
+    const h1 = document.createElement('h1');
+
+btn.addEventListener('click', function () {
+    if (email.value.length == 0 || pass.value.length == 0) {
+        h1.textContent = 'Hammasini to`ldirish kerak'
+        h1.style.color = 'red'
+        h1.style.fontSize = '20px';
+        document.body.appendChild(h1)
+        return
+        }
+    const res = `Login: ${email.value}        Password: ${pass.value}`
+    h1.textContent = res;
+    h1.style.color = 'red';
+    h1.style.fontSize = '20px';
+    document.body.appendChild(h1)
+})
