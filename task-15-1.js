8f3f673ccf9aa96da2ebe95ea0eae1dbeed2a321
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-06-11 20:18:46 +0500

    up

--- task-15-1.js
+++ task-15-1.js
@@ -16,8 +16,7 @@ async function getProductById(id) {
         const res = await fetch(url).then(res => res.json())
         for (let item of res) {
             if (item.id == id) {
-                console.log(`ID: ${item.id}\nName: ${item.name}\nPrice: ${item.price}\nQuantity: ${item.quantity}\nCatagory: ${item.catagory}`);
-                console.log("------------------------");
+                console.log(`ID: ${item.id}\nName: ${item.name}\nPrice: ${item.price}\nQuantity: ${item.quantity}\nCatagory: ${item.catagory}\n"------------------------"`);
             }
         }
     } catch (error) {
@@ -41,6 +40,7 @@ async function addProduct(name, price, quantity, catagory) {
             },
             body: JSON.stringify(newObj)
         })
+        console.log(`${name} maxsulot qo'shildi`);
     } catch (error) {
         console.log('Error on fetching users', error);
     }
@@ -54,15 +54,17 @@ async function updateProduct(id, obj) {
             },
             body: JSON.stringify(obj)
         });
+        console.log(`ID:${id}  maxsulot o'zgartirildi`);
     } catch (error) {
         console.log('Error on fetching users', error);
     }
 }
 async function removeProduct(id) {
     try {
-        const res = await fetch(`${url}/${id}`, {
+        await fetch(`${url}/${id}`, {
             method: "DELETE",
         });
+        console.log(`ID:${id}  maxsulot o'chirildi!`);
     } catch (error) {
         console.log('Error on fetching users', error);
     }
@@ -82,25 +84,53 @@ async function search(string) {
 }
 
 async function sortByCatagory() {
-
+    try {
+        const res = await fetch(url).then(res => res.json())
+        const sorted = [...res].sort((a, b) => b.catagory.localeCompare(a.catagory));
+        console.log("Sortlandi\n",sorted);
+    } catch (error) {
+        console.log('Error on fetching users', error);
+    }
 }
-
 async function sortByPrice() {
-
+    try {
+        const res = await fetch(url).then(res => res.json())
+        const sorted = [...res].sort((a, b) => a.price-b.price);
+        console.log("Sortlandi\n", sorted);
+    } catch (error) {
+        console.log('Error on fetching users', error);
+    }
 }
 
 async function getProductsByPrices(start, stop) {
-
+    try {
+        const res = await fetch(url).then(res => res.json())
+        for (let item of res) {
+            if (start <= item.price && stop >= item.price) {
+                console.log(item);
+            }
+        }
+    } catch (error) {
+        console.log('Error on fetching users', error);
+    }
 }
-// await addProduct("Iphone 13 Pro Max", 500, 20, "phone");
-// await addProduct("Samsung Galaxy S22", 450, 18, "phone");
-// await addProduct("MacBook Air M2", 1000, 24, "computer");
-// await addProduct("Asus ROG Strix", 1200, 24, "computer");
-// await addProduct("LG Smart TV 55", 700, 18, "tv");
-// await addProduct("Samsung QLED 65''", 1100, 24, "tv")
 
-// await getProducts()
-// await getProductById(3)
-// await updateProduct(3, { name: "Samsung 24 Ultra" })
-// await removeProduct(1)
-// await search('Sam')
+async function getAllResult() {
+    // await addProduct("Iphone 13 Pro Max", 500, 20, "phone");
+    // await addProduct("Samsung Galaxy S22", 450, 18, "phone");
+    // await addProduct("MacBook Air M2", 1000, 24, "computer");
+    // await addProduct("Asus ROG Strix", 1200, 24, "computer");
+    // await addProduct("LG Smart TV 55", 700, 18, "tv");
+    // await addProduct("Samsung QLED 65''", 1100, 24, "tv")
+
+    // await getProducts();
+    // await getProductById(3);
+    // await updateProduct(3, { name: "Samsung 24 Ultra" });
+    // await removeProduct(1);
+    // await search('Sam');
+    // await sortByCatagory()
+    // await sortByPrice()
+    await getProductsByPrices(200,800)
+}
+
+getAllResult()
