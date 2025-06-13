dde16f29f8306d277768f107f990cfd3f472e0c6
komol7390x <www.komol8689@gmail.com>
2025-06-13 17:44:12 +0500

    up

--- Task-1/task-17.js
+++ Task-1/task-17.js
@@ -1,7 +1,7 @@
 const url = 'https://dummyjson.com/products'
 class Product {
     constructor() {
-        this.data =[]
+        this.data = []
     }
     async urlInfo() {
         try {
@@ -19,12 +19,51 @@ class Product {
     }
 
     async getInfo() {
+        const takeRes = await this.urlInfo()
+        takeRes.forEach(val => {
+            const wrapper = document.getElementById('wrapper')
+            const constainer = document.createElement('div')
+            const title = document.createElement('h4')
+            const rating = document.createElement('span');
+            const stock = document.createElement('span');
+            const oldPrice = document.createElement('h5');
+            const newPrice = document.createElement('p')
+            const newPrice2 = document.createElement('p')
+
+            constainer.classList.add('container')
+            title.classList.add('productName');
+            rating.classList.add('rating');
+            stock.classList.add('stock');
+            oldPrice.classList.add('oldPrice');
+            newPrice.classList.add('newPrice')
+            newPrice2.classList.add('newPrice2')
+
+            const price1 = Math.floor(val.price * 1000)
+            const discount = Math.floor(price1 * (1 - (val.discountPercentage / 100)))
+
+            title.textContent = val.title
+            constainer.appendChild(title)
+
+            rating.textContent = `⭐️ ${val.rating}`
+            constainer.appendChild(rating)
+
+            stock.textContent = ` ( ${val.stock} rating)`
+            constainer.appendChild(stock)
+
+            newPrice.textContent = `${Math.floor(discount / 12)} sum/month`
+            constainer.appendChild(newPrice)
+
+            oldPrice.textContent = `${price1} sum`
+            constainer.appendChild(oldPrice)
+
+            newPrice2.textContent = `${discount} sum`
+            constainer.appendChild(newPrice2)
+
+            wrapper.appendChild(constainer)
+        })
+        return
         try {
-            const takeRes =await this.urlInfo()
-            for (let [key, val] of Object.entries(takeRes)) {
-                if (val.id==2)
-                console.log(val);
-            }
+
         } catch (error) {
             console.log(`Bangg2: ${error}`);
         }
