3c67c420d7a84ecaf1c7e69990d59c8431d083a1
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-06-16 21:30:19 +0500

    up

--- problem-19.js
+++ problem-19.js
@@ -1,4 +1,4 @@
-// TASK-1
+// TASK-26
 var removeDuplicates1 = function (nums) {
     let arr = [];
     for (let i = 0; i < nums.length; i++){
@@ -11,15 +11,40 @@ var removeDuplicates1 = function (nums) {
 var removeDuplicates2 = function (nums) {
     return [...new Set(nums)]
 };
-console.log(removeDuplicates1([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]));
-console.log(removeDuplicates2([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]));
+// console.log(removeDuplicates1([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]));
+// console.log(removeDuplicates2([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]));
 // ------------------------------------------------------------------
-// TASK-2
+// TASK-104
 var maxDepth = function (root) {
-    if (root === null) {
+    if (!root) return 0;
+    return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+};
+// console.log(maxDepth([3, 9, 20, null, null, 15, 7]));
+// ------------------------------------------------------------------
+// TASK - 217
+var containsDuplicate = function (nums) {
+    const len=[...new Set(nums)].length
+    return len==nums.length
+};
+// console.log(containsDuplicate([1, 2, 3,4,4]));
+// ------------------------------------------------------------------
+// TASK - 121
+var maxProfit = function (prices) {
+    if (prices.length == 0) {
         return 0;
     }
-    let left = maxDepth(root.left);
-    let right = maxDepth(root.right);
-    return 1 + Math.max(left, right);
+    let minPrice = prices[0];
+    let maxProfit = 0;
+    for (let i = 1; i < prices.length; i++) {
+        if (prices[i] < minPrice) {
+            minPrice = prices[i];
+        } else {
+            let profit = prices[i] - minPrice;
+            if (profit > maxProfit) {
+                maxProfit = profit;
+            }
+        }
+    }
+    return maxProfit;
 };
+
